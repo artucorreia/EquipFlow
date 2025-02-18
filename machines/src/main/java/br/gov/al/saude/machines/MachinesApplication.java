b82dd7ceba9557bbd43a3cc6cec84ffd4e5f3c8b
artucorreia <arthurcorria0109@gmail.com>
2025-02-18 16:36:24 -0300

    feat: add ms machines

---
+++ machines/src/main/java/br/gov/al/saude/machines/MachinesApplication.java
@@ -0,0 +1,13 @@
+package br.gov.al.saude.machines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinesApplication.class, args);
+	}
+
+}
---
+++ machines/src/test/java/br/gov/al/saude/machines/MachinesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.al.saude.machines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
